c0a75ef8e74a5cfa1a908fcb81a08ae5f8098842
Tyca, Michael (SE-A/53) <michael.tyca@volkswagen.de>
2020-03-05 15:39:43 +0100

    edited calendar view

--- src/app/calendarview/calendarview.component.ts
+++ src/app/calendarview/calendarview.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalendarviewComponent implements OnInit {
 
+  days: string[] = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"];
+
   weeks: number[][] = [[24,25,26,27,28,29,1],
                        [2,3,4,5,6,7,8],
                        [9,10,11,12,13,14,15],
